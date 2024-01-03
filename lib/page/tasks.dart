c3b29e690f6e930602fecf41a170c45d04800bb3
always-reach <67514209+always-reach@users.noreply.github.com>
2024-01-03 19:12:24 +0900

    一覧

--- lib/page/tasks.dart
+++ lib/page/tasks.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:todo/entity/task.dart';
 import 'package:todo/page/task_create.dart';
+import 'package:todo/repository/task_imp.dart';
 import 'package:todo/widget/task_item.dart';
 
 class TaskList extends StatelessWidget {
   const TaskList({super.key});
-  //static const tasks = ["task1", "task2", "task3"];
-  static const tasks = [];
 
   @override
   Widget build(BuildContext context) {
@@ -14,16 +14,26 @@ class TaskList extends StatelessWidget {
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
           title: const Text("My Mission"),
         ),
-        body: tasks.length > 0
-            ? ListView.builder(
+        body: FutureBuilder<List<Task>>(
+            future: TaskRepositry().getAllTasks(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              }
+              if (snapshot.hasError) {
+                return const Center(child: Text("エラーが発生しました"));
+              }
+              if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                return const Center(child: Text("データがありません"));
+              }
+              var tasks = snapshot.data!;
+              return ListView.builder(
                 itemCount: tasks.length,
                 itemBuilder: (context, index) {
-                  return taskItem(tasks[index]);
+                  return taskItem(tasks[index].title);
                 },
-              )
-            : const Center(
-                child: Text("タスクを追加してください。"),
-              ),
+              );
+            }),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
             Navigator.push(
