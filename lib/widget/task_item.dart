3b73bfab5d4989e3897566c87d5c0f23701a346d
always-reach <67514209+always-reach@users.noreply.github.com>
2024-01-14 02:55:50 +0900

    タスクに色とアイコンを

--- lib/widget/task_item.dart
+++ lib/widget/task_item.dart
@@ -4,7 +4,16 @@ import 'package:todo/page/task_detail.dart';
 
 Widget taskItem(Task task, BuildContext context) {
   return ListTile(
-    title: Text(task.title),
+    leading: task.isComplete
+        ? const Icon(Icons.done, color: Colors.green)
+        : const Icon(Icons.hourglass_empty),
+    subtitle: Text(
+      task.description ?? '',
+    ),
+    title: Text(task.title,
+        style: TextStyle(
+          color: task.isComplete ? Colors.grey : Colors.black,
+        )),
     onTap: () {
       Navigator.push(
         context,
@@ -13,5 +22,7 @@ Widget taskItem(Task task, BuildContext context) {
                 TaskDetailPage(key: UniqueKey(), id: task.id!)),
       );
     },
+    dense: true,
+    tileColor: task.isComplete ? Colors.grey[200] : Colors.white,
   );
 }
