e4d59dd4c6f5c4582ac46f434c8d19379df60088
always-reach <67514209+always-reach@users.noreply.github.com>
2024-01-24 22:59:01 +0900

    :art: プラットフォームごとにDBパスを設定

--- lib/helper/db_helper.dart
+++ lib/helper/db_helper.dart
@@ -1,10 +1,12 @@
+import 'dart:io';
+
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 class DatabaseHelper {
-  static const _databaseName = "sample.dbb";
-  static const _databaseVersion = 1;
+  static const _databaseName = "todo.db";
+  static const _databaseVersion = 2;
 
   DatabaseHelper._privateConstructor();
   static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
@@ -17,9 +19,15 @@ class DatabaseHelper {
   }
 
   Future<Database> _initDatabase() async {
-    final dbDirectory = await getApplicationSupportDirectory();
-    final dbFilePath = dbDirectory.path;
-    final path = join(dbFilePath, _databaseName);
+    var dbPath = "";
+    if (Platform.isAndroid) {
+      dbPath = await getDatabasesPath();
+    } else if (Platform.isIOS) {
+      dbPath = (await getLibraryDirectory()).path;
+    } else {
+      throw Exception("Platform not supported");
+    }
+    final path = join(dbPath, _databaseName);
 
     return await openDatabase(path,
         version: _databaseVersion, onCreate: _onCreate, onUpgrade: _onUPgrade);
